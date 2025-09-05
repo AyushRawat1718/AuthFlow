3b2047e1e5c589e3285daf4599ef5e98cf9da177
AyushRawat1718 <ayushrawat1718@gmail.com>
2025-09-06 01:00:04 +0530

    Adding some basic configs, along with CORS config

--- src/app.js
+++ src/app.js
@@ -1,6 +1,38 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+// Setting some basic configurations
+
+// Parse incoming JSON requests and limit payload size to 16kb
+app.use(express.json({ limit: "16kb" }));
+
+// Parse URL-encoded form data and allow nested objects, limit 16kb
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// Serve static files (images, CSS, JS, etc.) from the "public" folder
+app.use(express.static("public"));
+
+// CORS Configuration =>By enabling CORS we allow frontend apps on different domains/ports to access backend APIs securely, if we don't do this, our backend would not able to communicate with frontend and vice-versa
+
+/*
+// Enable CORS for all origins (default, allows any frontend)
+app.use(cors());
+*/
+
+app.use(
+  cors({
+    // Allow multiple origins from env (comma-separated) or fallback to localhost
+    origin: process.env.CORS_ORIGIN?.split(",") || "https://localhost:5173",
+
+    // Allow only specific HTTP methods for controlled access
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+
+    // Allow only specific headers like auth tokens and content type
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
 
 //Setting the home page route
 app.get("/", (req, res) => {
