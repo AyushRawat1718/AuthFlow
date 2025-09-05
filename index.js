cd30b48df5bb285e099816e6e9499090f085fc8e
AyushRawat1718 <ayushrawat1718@gmail.com>
2025-09-05 22:56:13 +0530

    Added .env file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Starting backend, with hitesh sir");
+//Importing the .env file, so that we could use that
+import dotenv from "dotenv";
+
+//Setting the path so that there is no problem in finding the env file
+dotenv.config({
+  path: "./.env",
+});
+
+let myUserName = process.env.MY_USERNAME;
+
+console.log(`Starting backend with ${myUserName}`);
