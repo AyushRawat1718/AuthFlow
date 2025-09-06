8f0230f38ca76d39e008bc66b739e7e0a42e57bf
AyushRawat1718 <ayushrawat1718@gmail.com>
2025-09-06 16:12:43 +0530

    Adding password verification method

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -61,10 +61,16 @@ const userSchema = new Schema(
   },
 );
 
+// Pre hook for password encryption
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+
+// Implementing the mongoose methods for password check
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
